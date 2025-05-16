de6c21e13c5b98029dce2c20fdf4cea7cd57852f
Tandrita Dewanjee <tandritadewanjee63@gmail.com>
2025-05-16 21:42:36 +0530

    Second Commit

---
+++ src/ex_1_Java_Basics/Lan002.java
@@ -0,0 +1,7 @@
+package ex_1_Java_Basics;
+
+public class Lan002 {
+    public static void main(String[] args) {
+        System.out.println("Package,public,class,static,void are the Keywords");
+    }
+}
