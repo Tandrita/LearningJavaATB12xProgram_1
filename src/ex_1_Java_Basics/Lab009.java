ece4a0db194d7e8377f1a165f6d116152dc3e849
Tandrita Dewanjee <tandritadewanjee63@gmail.com>
2025-05-18 15:32:00 +0530

    11th May Task 1

--- src/ex_1_Java_Basics/Lab009.java
+++ src/ex_1_Java_Basics/Lab009.java
@@ -2,18 +2,19 @@ package ex_1_Java_Basics;
 
 public class Lab009 {
     public static void main(String[] args) {
-        int num = 5;
-        System.out.printf("%d x 1 = %d", num,num*1).println();
-        System.out.printf("%d x 2 = %d",num,num*2).println();
-        System.out.printf("%d x 3 = %d",num,num*3).println();
-        System.out.println(".....");
-        System.out.printf("%d x 10 = %d",num,num*10).println();
         // Table of 5 you need to print,  (only use the printF)
         //int num = 5;
         //5×1=5
         //5×2 = 10
         //….
         //5×10 =50
+        int num = 5;
+        System.out.printf("%d x 1 = %d", num,num*1).println();
+        System.out.printf("%d x 2 = %d",num,num*2).println();
+        System.out.printf("%d x 3 = %d",num,num*3).println();
+        System.out.println(".....");
+        System.out.printf("%d x 10 = %d",num,num*10).println();
+
 
     }
 }
