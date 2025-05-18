5392e2927c6b4f96dedd9a22ca515feb41616922
Tandrita Dewanjee <tandritadewanjee63@gmail.com>
2025-05-18 11:55:24 +0530

    Main Method Overloading

---
+++ src/ex_1_Java_Basics/Lab005.java
@@ -0,0 +1,15 @@
+package ex_1_Java_Basics;
+
+public class Lab005 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int args) { // main method overloading
+
+    }
+
+    public static void main(String args) { // multi main method but jvm only excepts only one i.e the first one.
+
+    }
+}
