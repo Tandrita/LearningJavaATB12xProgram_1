2ae558b983388bad777f9e2c206adfdbe9cd8aa8
Tandrita Dewanjee <tandritadewanjee63@gmail.com>
2025-05-18 15:30:46 +0530

    Printf

---
+++ src/ex_1_Java_Basics/Lab008.java
@@ -0,0 +1,13 @@
+package ex_1_Java_Basics;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = 10+10;
+        System.out.printf("%d + %d =",a,b); // it prints in string format
+        System.out.println(c);
+        System.out.printf("%d", a/2);
+
+    }
+}
