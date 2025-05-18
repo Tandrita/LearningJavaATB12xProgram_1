27de84d6fc1894a485adc5c6a5ded3e1d3265ce5
Tandrita Dewanjee <tandritadewanjee63@gmail.com>
2025-05-18 11:37:51 +0530

    Maths

---
+++ src/ex_1_Java_Basics/Lab003.java
@@ -0,0 +1,13 @@
+package ex_1_Java_Basics;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(2+2*2);
+        System.out.println(2+2-2);
+        System.out.println(3/2); // println command always print integer value (No Decimal)
+    }
+}
